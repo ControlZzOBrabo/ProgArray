0a61667cbb8e90d8586725f0661cb0220d28d165
Andre <andre_baixo@estudante.sesisenai.org.br>
2023-03-15 08:11:57 -0300

    Arquivos iniciais HTML, CSS e JS
    ls -a

---
+++ vetor.js
@@ -0,0 +1,9 @@
+/*Foi declarada no lado de fora 
+da função para ser uma variável Global*/
+let cidade = ['Tijucas', 'Itapema', 1, 2, '@', '#']
+
+function inserir(){
+
+    console.log(cidade)
+
+}
