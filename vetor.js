26b801c8b084d50b34e55c24962c6ef06dacf3dd
Andre <andre_baixo@estudante.sesisenai.org.br>
2023-03-15 09:30:00 -0300

    zébuceta

--- vetor.js
+++ vetor.js
@@ -1,16 +1,17 @@
 /*Foi declarada no lado de fora 
 da função para ser uma variável Global*/
-let valor_cidade = ['Tijucas', 'Itapema', 1, 2, '@', '#']
+let valor_cidade = []
+let res = document.getElementById('res').value
 
 function inserir(){
 
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
-
+    res.innerHTML = valor_cidade
 }
 
-function excluír(){
+function excluir(){
     valor_cidade.pop()
     console.log(valor_cidade)
 
